61420204932fc2b352594e3cb2a3997a799d6099
LeoTheNrdKng <lionheljoela@gmail.com>
2023-07-14 10:29:37 -0700

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Prints the all single numbers of base 10.
+* Return: 0
+*/
+
+int main(void)
+{
+char justBase;
+char justBase2;
+
+for (justBase = '0' ; justBase <= '9' ; justBase++)
+{
+putchar(justBase);
+}
+for (justBase2 = 'a' ; justBase2 <= 'f' ; justBase2++)
+{
+putchar(justBase2);
+}
+putchar(10);
+return (0);
+}
