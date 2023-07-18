9b03319cae6d6cdfcbd7a0e7d0ed0163e7c2d236
LeoTheNrdKng <lionheljoela@gmail.com>
2023-07-18 11:03:50 -0700

    Several codes related to numbers and time

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - description
+* @n: number
+*/
+
+void print_to_98(int n)
+{
+for (; n > 98; n--)
+printf("%d, ", n);
+for (; n < 98; n++)
+printf("%d, ", n);
+printf("98\n");
+}
