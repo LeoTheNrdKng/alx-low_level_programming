fccd2b2f7063803201cc68337a941f646a9598be
LeoTheNrdKng <lionheljoela@gmail.com>
2023-07-18 10:09:20 -0700

    Write a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 
 /**
 * _putchar - writes a program that prints _putchar, followed by a new line.
-* pr_alph - writes a function that prints the alphabet, in lowercase, followed by a new line.
+* print_alphabet - writes a function that prints the alphabet, in lowercase, followed by a new line.
 */
 
 int _putchar(char);
