e2fcbce31e494a602fb2288f03fcb2d5c47a2d6e
LeoTheNrdKng <lionheljoela@gmail.com>
2023-07-20 15:53:49 -0700

    Nested loops and functions commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_diagonal - Print a diagonal in the terminal.
+* @n: numbers of lines.
+*/
+
+void print_diagonal(int n)
+{
+int spc, init = n;
+
+if (n <= 0)
+_putchar('\n');
+for (; n > 0; n--)
+{
+for (spc = init - n; spc > 0; spc--)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains more nested loops and functions.
