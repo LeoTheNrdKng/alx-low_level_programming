fc8a3b009a4dcac6598bc7fc354b3df87e542d1b
LeoTheNrdKng <lionheljoela@gmail.com>
2023-07-27 06:57:46 -0700

    Write a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,36 +1,57 @@
+/*
+ * File: 103-print_buffer.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
+#include <stdio.h>
+
 /**
-* print_buffer - entry point
-* @b: arguments
-* @size: size of buffer
-*/
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
+ */
 void print_buffer(char *b, int size)
 {
-int i, j, k;
+int byte, index;
 
-for (i = 0; i < size; i += 10)
+for (byte = 0; byte < size; byte += 10)
 {
-printf("%08x: ", i);
-k = (i / 10 == size / 10) ? size % 10 : 10;
-for (j = 0; j < k; j++)
+printf("%08x: ", byte);
+
+for (index = 0; index < 10; index++)
 {
-if (!(j % 2))
-printf("%02x", b[i + j]);
-else
-printf("%02x ", b[i + j]);
-}
-while (j < 10)
-{
-if (!(j % 2))
+if ((index + byte) >= size)
 printf("  ");
+
 else
-printf("   ");
-j++;
+printf("%02x", *(b + index + byte));
+
+if ((index % 2) != 0 && index != 0)
+printf(" ");
 }
-for (j = 0; j < k; j++)
-printf("%c", b[i + j] < 32 ? '.' : b[i + j]);
+
+for (index = 0; index < 10; index++)
+{
+if ((index + byte) >= size)
+break;
+
+else if (*(b + index + byte) >= 31 &&
+*(b + index + byte) <= 126)
+printf("%c", *(b + index + byte));
+
+else
+printf(".");
+}
+
+if (byte >= size)
+continue;
+
 printf("\n");
 }
+
 if (size <= 0)
 printf("\n");
 }
