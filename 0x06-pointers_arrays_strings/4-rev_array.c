ccbcbfc9742fb188e4b40e4d5bc0a9fcab98ffb2
LeoTheNrdKng <lionheljoela@gmail.com>
2023-07-27 06:10:41 -0700

    Scripts for pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* reverse_array - Reverse the content of an array of integers.
+* @a: Array.
+* @n: n numbers of elements.
+*/
+
+void reverse_array(int *a, int n)
+{
+int i, j, tmp;
+
+j = n - 1;
+
+for (i = 0; i < j; i++, j--)
+{
+tmp = a[i];
+a[i] = a[j];
+a[j] = tmp;
+}
+}
