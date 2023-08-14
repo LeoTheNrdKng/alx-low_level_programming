0430885eb900c0ba7eeccc489cf688e78cde5daa
LeoTheNrdKng <lionheljoela@gmail.com>
2023-08-14 23:33:54 +0200

    Updated this file with scripts for preprocessors.

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _MACRO_H
+#define _MACRO_H
+
+#define SIZE 1024
+
+#endif /* _MACRO_H */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _LIKE_MACRO_
+#define _LIKE_MACRO_
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+#endif
